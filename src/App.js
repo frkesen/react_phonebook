aae8778e209ac71f606bc8ba4ca71a33557923f6
Faruk <felixfranko66@gmail.com>
2020-12-12 02:58:37 +0300

    app created

---
+++ src/App.js
@@ -0,0 +1,158 @@
+import React, { useState } from 'react';
+
+const style = {
+  table: {
+    borderCollapse: 'collapse'
+  },
+  tableCell: {
+    border: '1px solid gray',
+    margin: 0,
+    padding: '5px 10px',
+    width: 'max-content',
+    minWidth: '150px'
+  },
+  form: {
+    container: {
+      padding: '20px',
+      border: '1px solid #F0F8FF',
+      borderRadius: '15px',
+      width: 'max-content',
+      marginBottom: '40px'
+    },
+    inputs: {
+      marginBottom: '5px'
+    },
+    submitBtn: {
+      marginTop: '10px',
+      padding: '10px 15px',
+      border:'none',
+      backgroundColor: 'lightseagreen',
+      fontSize: '14px',
+      borderRadius: '5px'
+    }
+  }
+}
+function PhoneBookForm({ handleNameChange, handleLastNameChange, handlePhoneChange, handleSubmit}) {
+  return (
+    <form onSubmit={handleSubmit} style={style.form.container}>
+      <label>First name:</label>
+      <br />
+      <input 
+        style={style.form.inputs}
+        className='userFirstname'
+        name='userFirstname' 
+        type='text'
+        placeholder='Cooder'
+        onChange={handleNameChange}
+        
+      />
+      <br/>
+      <label>Last name:</label>
+      <br />
+      <input 
+        style={style.form.inputs}
+        className='userLastname'
+        name='userLastname' 
+        type='text' 
+        placeholder='Byte'
+        onChange={handleLastNameChange}
+      />
+      <br />
+      <label>Phone:</label>
+      <br />
+      <input
+        style={style.form.inputs}
+        className='userPhone' 
+        name='userPhone' 
+        type='text'
+        placeholder='8885559999'
+        onChange={handlePhoneChange}
+      />
+      <br/>
+      <input 
+        style={style.form.submitBtn} 
+        className='submitButton'
+        type='submit' 
+        value='Add User'
+      />
+    </form>
+  )
+}
+function InformationTable({info}) {
+  console.log(info);
+  return (
+    <table style={style.table} className='informationTable'>
+      <thead> 
+        <tr>
+          <th style={style.tableCell}>First name</th>
+          <th style={style.tableCell}>Last name</th>
+          <th style={style.tableCell}>Phone</th>
+        </tr>
+        {info?.map(
+        (table, index) => (
+          <tr key={index} >
+            <td style={style.tableCell}>{table.name}</td>
+            <td style={style.tableCell}>{table.lastName}</td>
+            <td style={style.tableCell}>{table.phone}</td>
+        </tr>
+        )
+      )}
+      </thead> 
+    </table>
+  );
+};
+
+function App(props) {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [info, setInfo] = useState([]);
+
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value)
+    
+  };
+
+  const handlePhoneChange = (e) => {
+    setPhone(e.target.value)
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newInfo = info.concat({ name, lastName, phone });
+    function compare( a, b ) {
+      let first = (a.lastName).toLowerCase();
+      let second = (b.lastName).toLowerCase();
+
+      if ( first < second ){
+        return -1;
+      }
+      if ( first > second ){
+        return 1;
+      }
+      return 0;
+    }
+    newInfo.sort( compare );
+    setInfo(newInfo);
+  };
+
+  return (
+    <section>
+      <PhoneBookForm 
+        handleNameChange={handleNameChange}
+        handleLastNameChange={handleLastNameChange}
+        handlePhoneChange={handlePhoneChange}
+        handleSubmit={handleSubmit}
+        
+      />
+      <InformationTable info={info} />
+    </section>
+  );
+};
+
+export default App;
